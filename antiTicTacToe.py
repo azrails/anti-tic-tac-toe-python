f707329dab34a05b23b29c8d68bb6662cfafb47d
azrail <wsallei@student.21-school.ru>
2021-10-17 23:48:39 +0300

    f

--- antiTicTacToe.py
+++ antiTicTacToe.py
@@ -137,6 +137,11 @@ def tictactoe(size_of_map: int, size_to_victory: int)-> None:
 
 @click.command()
 def main():
+    """Anti tic-tac-toe program.
+    Loses the one who collects 5 in a row \ diagonal \ column in box 10x10 (standart).
+    Also, in the case of a function call in other modules, you can set the dimension of the playing field n and the victory condition.
+    the computer uses a naive algorithm of an ideal player - the main goal is not to get a loss, 
+    since it has been statistically proven that on large fields a person is not able to keep the whole field in his head, which leads to defeat"""
     tictactoe(10, 5)
 
 if __name__ == '__main__':
